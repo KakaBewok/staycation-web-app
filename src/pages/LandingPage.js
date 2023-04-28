5a99143272248c07b63091624d17586ca90ac36b
kkbwk <rizalnov667@gmail.com>
2023-04-29 01:10:52 +0700

    fixing most picked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
 import landingPage from '../json/landingPage.json';
 
 const LandingPage = (props) => {
   return (
     <>
-      <Header {...props}></Header>
-      <Hero data={landingPage.hero}></Hero>
-      {/* <Hero></Hero> */}
+      <Header {...props} />
+      <Hero data={landingPage.hero} />
+      <MostPicked data={landingPage.mostPicked} />
     </>
   );
 };
