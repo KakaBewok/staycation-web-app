217b1648d0a8a7d530db7a403d17350378f09f81
KakaBewok <rizalnov667@gmail.com>
2023-07-06 22:41:36 +0700

    fixed views

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+//route admin
+const adminRouter = require("./routes/admin");
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
