3677c979835db2d05b28a1aa44785526f191fe9e
KakaBewok <rizalnov667@gmail.com>
2023-07-10 00:00:21 +0700

    CRUD admin category

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+//custom import
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/staycation")
+  .then(() => console.log("MongoDB Connected ..."))
+  .catch((err) => console.log(err));
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -15,6 +23,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,65 @@
+const Category = require("../models/Category");
+
 module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard", {
-      title: "Admin | Dashboard",
+      title: "Staycation | Dashboard",
+    });
+  },
+
+  //Category
+  viewCategory: async (req, res) => {
+    const category = await Category.find();
+
+    res.render("admin/category/view_category", {
+      title: "Staycation | Category",
+      category,
+    });
+  },
+  addCategory: async (req, res) => {
+    const { name } = req.body;
+
+    await Category.create({ name });
+    res.redirect("/admin/category");
+  },
+  editCategory: async (req, res) => {
+    const { id, name } = req.body;
+
+    const category = await Category.findOne({ _id: id });
+
+    // Update data category name from db with req payload
+    category.name = name;
+
+    // Then save the data
+    await category.save();
+
+    // Then redirect to category page
+    res.redirect("/admin/category");
+  },
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+
+    category.deleteOne();
+
+    res.redirect("/admin/category");
+  },
+
+  viewBank: (req, res) => {
+    res.render("admin/bank/view_bank", {
+      title: "Staycation | Bank",
+    });
+  },
+
+  viewItem: (req, res) => {
+    res.render("admin/item/view_item", {
+      title: "Staycation | Item",
+    });
+  },
+
+  viewBooking: (req, res) => {
+    res.render("admin/booking/view_booking", {
+      title: "Staycation | Booking",
     });
   },
 };
