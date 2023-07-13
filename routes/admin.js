a686fe41cff973bd82ff2aba9e249d43084b7a85
KakaBewok <rizalnov667@gmail.com>
2023-07-14 00:02:22 +0700

    delete bank

--- routes/admin.js
+++ routes/admin.js
@@ -4,16 +4,17 @@ const { uploadSingle } = require("../middleware/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
-// category
+// category endpoint
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
 
-//bank
+//bank endpoint
 router.get("/bank", adminController.viewBank);
 router.post("/bank", uploadSingle, adminController.addBank);
 router.put("/bank", uploadSingle, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
 
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
