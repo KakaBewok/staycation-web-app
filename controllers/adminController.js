f08994df439b6a985eea878faf4d8821bbe786e0
KakaBewok <rizalnov667@gmail.com>
2023-08-04 07:56:18 +0700

    add crud item and sign in

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,14 +4,62 @@ const Item = require("../models/Item");
 const Image = require("../models/Image");
 const Feature = require("../models/Feature");
 const Activity = require("../models/Activity");
+const Users = require("../models/Users");
 const fs = require("fs-extra");
 const path = require("path");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard", {
       title: "Staycation | Dashboard",
     });
+  },
+
+  //Sign in
+  viewSignin: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+      if (req.session.user == null || req.session.user == undefined) {
+        res.render("index", {
+          alert,
+          title: "Staycation | Login",
+        });
+      } else {
+        res.redirect("/admin/dashboard");
+      }
+    } catch (error) {
+      res.redirect("/admin/signin");
+    }
+  },
+  actionSignin: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await Users.findOne({ username: username });
+      if (!user) {
+        req.flash("alertMessage", "User not found!!");
+        req.flash("alertStatus", "danger");
+        res.redirect("/admin/signin");
+      }
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        req.flash("alertMessage", "Password don't match!");
+        req.flash("alertStatus", "danger");
+        res.redirect("/admin/signin");
+      }
+
+      req.session.user = {
+        //from db
+        id: user.id,
+        username: user.username,
+      };
+
+      res.redirect("/admin/dashboard");
+    } catch (error) {
+      res.redirect("/admin/signin");
+    }
   },
 
   //Category
@@ -368,7 +416,6 @@ module.exports = {
       res.redirect("/admin/item");
     }
   },
-
   //detail item
   viewDetailItem: async (req, res) => {
     const { itemId } = req.params;
@@ -422,7 +469,6 @@ module.exports = {
       res.redirect(`/admin/item/show-detail-item/${itemId}`);
     }
   },
-  //sampe sini
   editFeature: async (req, res) => {
     const { id, name, qty, itemId } = req.body;
     try {
@@ -450,6 +496,29 @@ module.exports = {
       res.redirect(`/admin/item/show-detail-item/${itemId}`);
     }
   },
+  deleteFeature: async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+      const feature = await Feature.findOne({ _id: id });
+
+      const item = await Item.findOne({ _id: itemId }).populate("featureId");
+      for (let i = 0; i < item.featureId.length; i++) {
+        if (item.featureId[i]._id.toString() === feature._id.toString()) {
+          item.featureId.pull({ _id: feature._id });
+          await item.save();
+        }
+      }
+      await fs.unlink(path.join(`public/${feature.imageUrl}`));
+      await feature.deleteOne();
+      req.flash("alertMessage", "Success Delete Feature");
+      req.flash("alertStatus", "success");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
   addActivity: async (req, res) => {
     const { name, type, itemId } = req.body;
 
@@ -471,6 +540,56 @@ module.exports = {
       await item.save();
 
       req.flash("alertMessage", "Success Add Activity");
+      req.flash("alertStatus", "success");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+  editActivity: async (req, res) => {
+    const { id, name, type, itemId } = req.body;
+    try {
+      const activity = await Activity.findOne({ _id: id });
+      if (req.file == undefined) {
+        activity.name = name;
+        activity.type = type;
+        await activity.save();
+        req.flash("alertMessage", "Success Update activity");
+        req.flash("alertStatus", "success");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      } else {
+        await fs.unlink(path.join(`public/${activity.imageUrl}`));
+        activity.name = name;
+        activity.type = type;
+        activity.imageUrl = `images/${req.file.filename}`;
+        await activity.save();
+        req.flash("alertMessage", "Success Update activity");
+        req.flash("alertStatus", "success");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+  deleteActivity: async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+      const activity = await Activity.findOne({ _id: id });
+
+      const item = await Item.findOne({ _id: itemId }).populate("activityId");
+      for (let i = 0; i < item.activityId.length; i++) {
+        if (item.activityId[i]._id.toString() === activity._id.toString()) {
+          item.activityId.pull({ _id: activity._id });
+          await item.save();
+        }
+      }
+      await fs.unlink(path.join(`public/${activity.imageUrl}`));
+      await activity.deleteOne();
+      req.flash("alertMessage", "Success Delete Activity");
       req.flash("alertStatus", "success");
       res.redirect(`/admin/item/show-detail-item/${itemId}`);
     } catch (error) {
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,13 @@ const { uploadSingle, uploadMultiple } = require("../middleware/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
+// sign in endpoint
+router.get("/signin", adminController.viewSignin);
+router.post("/signin", adminController.actionSignin);
+// router.use(auth);
+// router.get("/logout", adminController.actionLogout);
+// router.get("/dashboard", adminController.viewDashboard);
+
 // category endpoint
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
@@ -29,9 +36,11 @@ router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
 
 router.post("/item/add/feature", uploadSingle, adminController.addFeature);
 router.put("/item/update/feature", uploadSingle, adminController.editFeature);
-// router.delete("/item/:itemId/feature/:id", adminController.deleteFeature);
+router.delete("/item/:itemId/feature/:id", adminController.deleteFeature);
 
 router.post("/item/add/activity", uploadSingle, adminController.addActivity);
+router.put("/item/update/activity", uploadSingle, adminController.editActivity);
+router.delete("/item/:itemId/activity/:id", adminController.deleteActivity);
 
 // booking endpoint
 router.get("/booking", adminController.viewBooking);
